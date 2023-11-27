5ddc706e5415ee5dac1450a386aa89cca1d89972
Joshua Martinelle <contact@jomar.fr>
2023-11-27 18:51:18 +0100

    Set private_class_method

--- libs/platforms/yeswehack/auth.rb
+++ libs/platforms/yeswehack/auth.rb
@@ -3,6 +3,11 @@
 module ScopesExtractor
   # YesWeHack
   module YesWeHack
+    LOGIN_URL = 'https://api.yeswehack.com/login'
+    OTP_LOGIN_URL = 'https://api.yeswehack.com/account/totp'
+
+    private_class_method :extract_totp_token, :extract_jwt
+
     def self.authenticate(config)
       totp_token = extract_totp_token(config)
       return unless totp_token
@@ -18,7 +23,7 @@ module ScopesExtractor
     def self.extract_totp_token(config)
       body = { email: config[:email], password: config[:password] }.to_json
 
-      response = HttpClient.post('https://api.yeswehack.com/login', { body: body })
+      response = HttpClient.post(LOGIN_URL, { body: body })
       return unless response&.code == 200
 
       json = Parser.json_parse(response.body)
@@ -31,7 +36,7 @@ module ScopesExtractor
       otp_code = ROTP::TOTP.new(config[:otp]).now
       body = { token: totp_token, code: otp_code }.to_json
 
-      response = HttpClient.post('https://api.yeswehack.com/account/totp', { body: body })
+      response = HttpClient.post(OTP_LOGIN_URL, { body: body })
       return unless response.code == 200
 
       json = Parser.json_parse(response.body)
