20f81565e157a93a502616e0d49d2d939f3e2f68
JoMar <contact@jomar.fr>
2023-11-28 21:34:38 +0100

    Remove unused logger

--- libs/utilities/logger.rb
+++ libs/utilities/logger.rb
@@ -11,25 +11,6 @@ module ScopesExtractor
       @logger ||= Logger.new($stdout)
     end
 
-    # Set the log level for the previous logger
-    def self.log_level=(level)
-      logger.level = level.downcase.to_sym
-    end
-
-    def self.log_fatal(message)
-      logger.fatal(message.red)
-
-      exit
-    end
-
-    def self.log_info(message)
-      logger.info(message.green)
-    end
-
-    def self.log_control(message)
-      logger.info(message.light_blue)
-    end
-
     def self.log_warn(message)
       logger.warn(message.yellow)
     end
