8f95446d7141848ab139e343d9690d6c9cad8ef6
MRTSec <contact@jomar.fr>
2024-06-26 18:26:21 +0200

    Improve YWH normalization

--- libs/platforms/yeswehack/scopes.rb
+++ libs/platforms/yeswehack/scopes.rb
@@ -41,7 +41,7 @@ module ScopesExtractor
 
       def self.add_scope_to_category(scopes, category, infos)
         if category == :url
-          normalize_urls(infos['scope']).each { |url| scopes[category] << url }
+          normalize(infos['scope'])&.each { |url| scopes[category] << url }
         else
           scopes[category] << infos['scope']
         end
@@ -54,35 +54,62 @@ module ScopesExtractor
         category
       end
 
-      def self.normalize_urls(scope)
-        normalized_urls = []
-        scope = sanitize_scope(scope)
+      def self.normalize(scope)
+        normalized_scopes = []
+        scope.sub!(/https?:\/\//, '') if scope.match?(/https?:\/\/\*\./)
+        scope = scope.split(' ').first
+        scope = scope[..-2] if scope.end_with?('/*')
+        scope = "*#{scope}" if scope.start_with?('.')
 
-        if (match_data = scope.match(/^\((.*)\)(.*)/))
-          normalized_urls.concat(normalize_with_subdomains(match_data[1], match_data[2]))
-        elsif (match_data = scope.match(/^(.*)[\[(]([\w|]+)/))
-          normalized_urls.concat(normalize_with_tlds(match_data[1], match_data[2]))
-        elsif scope.match?(%r{https?://\*})
-          normalized_urls << scope.sub(%r{https?://}, '')
-        elsif scope.match?(%r{^(https?://|\*\.)[/\w.\-?#!%:=]+$}) || scope.match?(%r{^[/\w.-]+\.[a-z]+(/.*)?}i)
-          normalized_urls << scope
-        else
-          Utilities.log_warn("YesWeHack - Non-normalized scope : #{scope}")
+        if (match = scope.match(/(?:https?:\/\/)?(?<prefix>\w+-)?\((?<subs>.*)\)(?<domain>.*)/))
+          normalized = normalize_with_subdomains(match)
+          normalized_scopes.concat(normalized)
         end
 
-        normalized_urls.uniq
+        if (match = scope.match(/^(?<base>.*)[\[(](?<tlds>[\w|]+)/))
+          normalized = normalize_with_tlds(match)
+          normalized_scopes.concat(normalized)
+        end
+
+        normalized_scopes << scope if normalized_scopes.empty?
+
+        normalized_scopes.uniq!
+        normalized_scopes.select do |s|
+          unless scope_valid?(s)
+            log_and_return(s)
+            false
+          else
+            true
+          end
+        end
       end
 
-      def self.sanitize_scope(scope)
-        scope.split(' ')[0].gsub(%r{(/\*|\)/)$}, '')
+      def self.scope_valid?(scope)
+        invalid_chars = [',', '{', '<', '[', '(', ' ']
+        if invalid_chars.any? { |char| scope.include?(char) } || !scope.include?('.')
+          false
+        else
+          true
+        end
       end
 
-      def self.normalize_with_subdomains(subs, domain)
-        subs.split('|').map { |sub| "#{sub}#{domain}" }
+      def self.log_and_return(scope)
+        Utilities.log_warn("YesWeHack - Non-normalized scope : #{scope}")
+        nil
       end
 
-      def self.normalize_with_tlds(base, tlds)
-        tlds.split('|').map { |tld| "#{base}#{tld}" }
+      def self.normalize_with_subdomains(match)
+        return [] if match[:subs]&.empty? || match[:domain]&.empty? || match[:domain] == '/'
+
+        subs = match[:subs]
+        subs.split('|').map { |sub| "#{match[:prefix]}#{sub}#{match[:domain]}" }
+      end
+
+      def self.normalize_with_tlds(match)
+        return [] if match[:tlds]&.empty? || match[:base]&.empty? || match[:base].match?(/^https:\/\/(?:api-)?$/)
+
+        tlds = match[:tlds]
+        tlds.split('|').map { |tld| "#{match[:base]}#{tld}" }
       end
     end
   end
