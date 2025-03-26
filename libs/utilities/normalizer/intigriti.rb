d6fdea69f9ec7cd55bbc7573238e1828b7507b43
Joshua MARTINELLE <contact@jomar.fr>
2025-03-26 19:41:46 +0100

    Add Intigriti

---
+++ libs/utilities/normalizer/intigriti.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module ScopesExtractor
+  # Normalizer module for standardizing domain formats
+  module Normalizer
+    # Intigriti module provides specialized normalization functions for Intigriti platform scopes
+    module Intigriti
+      # Normalize a domain scope string into standardized format(s)
+      # @param value [String] The raw domain scope string
+      # @return [Array<String>] List of normalized domain scopes
+      def self.normalization(value)
+        value.strip!
+
+        [value]
+      end
+    end
+  end
+end
