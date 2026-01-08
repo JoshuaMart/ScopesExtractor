95e58a417b026932da94dbbbf6a63aa2b35ce625
Joshua MARTINELLE <contact@jomar.fr>
2026-01-08 17:32:48 +0100

    Update scope.rb

--- ruby_migration/lib/scopes_extractor/models/scope.rb
+++ ruby_migration/lib/scopes_extractor/models/scope.rb
@@ -9,6 +9,16 @@ module ScopesExtractor
       attribute :type, Types::String.enum('web', 'mobile', 'api', 'source_code', 'executable', 'other')
       attribute :is_in_scope, Types::Bool.default(true)
       attribute? :created_at, Types::DateTime
+
+      def self.new(attributes)
+        # Auto-heuristic: If it looks like a wildcard domain, force type to 'web'
+        if attributes[:value].to_s.start_with?('*.')
+          attributes = attributes.dup # Avoid mutating original hash
+          attributes[:type] = 'web'
+        end
+
+        super(attributes)
+      end
     end
   end
 end
