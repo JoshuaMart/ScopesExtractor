84ef75f164640a380bdf5663ccb84c2e51ee86e8
Joshua MARTINELLE <contact@jomar.fr>
2026-01-08 17:46:09 +0100

    Update validator_spec.rb

--- ruby_migration/spec/scopes_extractor/validator_spec.rb
+++ ruby_migration/spec/scopes_extractor/validator_spec.rb
@@ -51,6 +51,18 @@ RSpec.describe ScopesExtractor::Validator do
         expect(described_class.valid_web_target?('*.example.com (description text)', 'web')).to be false
       end
 
+      it 'rejects template placeholders' do
+        expect(described_class.valid_web_target?('service-%username%-1.example.com', 'web')).to be false
+        expect(described_class.valid_web_target?('service-pam-###.example.com', 'web')).to be false
+        expect(described_class.valid_web_target?('api-{id}.example.com', 'web')).to be false
+      end
+
+      it 'allows hash in URL fragments but not in domain' do
+        expect(described_class.valid_web_target?('https://example.com/app/#/dashboard', 'web')).to be true
+        expect(described_class.valid_web_target?('example.com/#/dashboard', 'web')).to be true # Now allowed (hash in path)
+        expect(described_class.valid_web_target?('site-###.com', 'web')).to be false
+      end
+
       it 'rejects very short values' do
         expect(described_class.valid_web_target?('a.b', 'web')).to be false
       end
