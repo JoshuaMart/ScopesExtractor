53c966cd68a7c08a19342d6e94679e3f05ead275
Joshua MARTINELLE <contact@jomar.fr>
2026-01-10 19:05:37 +0100

    Refactor - Trash old files

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM --platform=linux/amd64 ruby:3.4.7
-
-WORKDIR /app
-COPY . .
-COPY Gemfile Gemfile
-
-RUN bundle install
-
-CMD ruby bin/main.rb
--- bin/main.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-require_relative '../libs/scopes_extractor/'
-
-extractor = ScopesExtractor::Extract.new
-extractor.run
